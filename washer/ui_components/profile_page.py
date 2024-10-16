bc06032480b046adad90dea33a872a8996d44239
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2024-10-17 02:25:59 +0500

    improve picture selection by adding true url logic & add booking visibility in tabs

--- washer/ui_components/profile_page.py
+++ washer/ui_components/profile_page.py
@@ -1,3 +1,6 @@
+import io
+import json
+
 import flet as ft
 import httpx
 
@@ -11,10 +14,15 @@ class ProfilePage:
         self.api_url = config.api_url
         self.username = self.page.client_storage.get('username')
 
+        self.avatar_url = self.page.client_storage.get('avatar_url')
+        if not self.avatar_url:
+            self.get_user_data()
+
         self.cars = []
         self.load_user_cars_from_server()
 
-        self.bookings = self.page.client_storage.get('bookings') or []
+        self.bookings = []
+        self.load_user_bookings_from_server()
 
         self.avatar_container = self.create_avatar_container()
         self.file_picker = self.create_file_picker()
@@ -22,6 +30,12 @@ class ProfilePage:
 
         page.clean()
         page.add(self.create_profile_page())
+
+    def on_login_success(self):
+        self.get_user_data()
+        self.page.clean()
+        self.page.add(self.create_profile_page())
+        self.page.update()
 
     def load_user_cars_from_server(self):
         """Загрузка автомобилей пользователя с сервера"""
@@ -62,6 +76,46 @@ class ProfilePage:
         except Exception as e:
             print(f'Ошибка при запросе автомобилей с сервера: {e}')
 
+    def load_user_bookings_from_server(self):
+        try:
+            access_token = self.page.client_storage.get('access_token')
+            user_id = self.page.client_storage.get('user_id')
+
+            if not user_id:
+                print('User ID not found!')
+                return
+
+            headers = {
+                'Authorization': f'Bearer {access_token}',
+                'Accept': 'application/json',
+            }
+
+            url = (
+                f'{self.api_url.rstrip("/")}/car_washes/bookings'
+                f'?user_id={user_id}&limit=100'
+            )
+
+            response = httpx.get(url, headers=headers)
+
+            if response.status_code == 200:
+                bookings = response.json().get('data', [])
+                if bookings:
+                    print(f'Букинги успешно загружены: {bookings}')
+                    self.bookings = bookings
+                    self.page.client_storage.set(
+                        f'bookings_{self.username}', bookings
+                    )
+                else:
+                    print('Букинги не найдены на сервере.')
+                    self.bookings = []
+            else:
+                print(
+                    f'Ошибка при загрузке букингов: '
+                    f'{response.status_code} - {response.text}'
+                )
+        except Exception as e:
+            print(f'Ошибка при запросе букингов с сервера: {e}')
+
     def create_tabs(self):
         return ft.Tabs(
             tabs=[
@@ -100,6 +154,7 @@ class ProfilePage:
                             spacing=15,
                             alignment=ft.MainAxisAlignment.CENTER,
                             horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                            scroll='adaptive',
                         ),
                         height=400,
                     ),
@@ -233,7 +288,28 @@ class ProfilePage:
 
     def create_booking_blocks(self):
         booking_blocks = []
-        for index, booking in enumerate(self.bookings):
+
+        if not self.bookings:
+            return [
+                ft.Container(
+                    content=ft.Text(
+                        'Тут будут отображаться ваши записи...',
+                        size=14,
+                        color=ft.colors.GREY_500,
+                        weight=ft.FontWeight.NORMAL,
+                    ),
+                    alignment=ft.alignment.center,
+                    expand=True,
+                    height=400,
+                )
+            ]
+
+        for _index, booking in enumerate(self.bookings):
+            booking_date = booking['start_datetime'].split('T')[0]
+            booking_time = booking['start_datetime'].split('T')[1][:5]
+
+            formatted_date = self.format_date(booking_date)
+
             booking_block = ft.Card(
                 content=ft.Container(
                     content=ft.Row(
@@ -242,15 +318,13 @@ class ProfilePage:
                                 content=ft.Column(
                                     controls=[
                                         ft.Text(
-                                            f"Дата записи: {booking['date']}",
+                                            f'Дата: {formatted_date}',
                                             size=16,
                                             weight=ft.FontWeight.BOLD,
                                         ),
+                                        ft.Text(f'Время: {booking_time}'),
                                         ft.Text(
-                                            f"Место: {booking['location']}"
-                                        ),
-                                        ft.Text(
-                                            f"Услуга: {booking['service']}"
+                                            f"Стоимость: {booking['price']} ₸"
                                         ),
                                     ],
                                     spacing=5,
@@ -260,7 +334,9 @@ class ProfilePage:
                             ft.IconButton(
                                 icon=ft.icons.DELETE,
                                 on_click=lambda e,
-                                i=index: self.on_delete_booking(i),
+                                booking_id=booking['id']: self.delete_booking(
+                                    booking_id
+                                ),
                                 icon_color=ft.colors.RED,
                                 padding=ft.padding.only(left=10, bottom=35),
                             ),
@@ -276,12 +352,94 @@ class ProfilePage:
                 elevation=2,
             )
             booking_blocks.append(booking_block)
+
         return booking_blocks
 
+    def format_date(self, booking_date):
+        import datetime
+
+        months = {
+            1: 'января',
+            2: 'февраля',
+            3: 'марта',
+            4: 'апреля',
+            5: 'мая',
+            6: 'июня',
+            7: 'июля',
+            8: 'августа',
+            9: 'сентября',
+            10: 'октября',
+            11: 'ноября',
+            12: 'декабря',
+        }
+        date_obj = datetime.datetime.strptime(booking_date, '%Y-%m-%d')
+        return f'{date_obj.day} {months[date_obj.month]}'
+
+    def load_location_by_id(self, location_id):
+        try:
+            access_token = self.page.client_storage.get('access_token')
+
+            headers = {
+                'Authorization': f'Bearer {access_token}',
+                'Accept': 'application/json',
+            }
+
+            url = f"{self.api_url.rstrip('/')}/locations/{location_id}"
+
+            response = httpx.get(url, headers=headers)
+
+            if response.status_code == 200:
+                location = response.json().get('data', {})
+                return location
+            else:
+                print(
+                    f'Ошибка при загрузке данных автомойки: '
+                    f'{response.status_code}'
+                )
+                return {}
+        except Exception as e:
+            print(f'Ошибка при запросе данных автомойки: {e}')
+            return {}
+
+    def delete_booking(self, booking_id):
+        try:
+            access_token = self.page.client_storage.get('access_token')
+            if not access_token:
+                print('Access token не найден.')
+                return
+
+            headers = {
+                'Authorization': f'Bearer {access_token}',
+                'Accept': 'application/json',
+            }
+
+            url = (
+                f'{self.api_url.rstrip("/")}/car_washes/bookings/{booking_id}'
+            )
+
+            response = httpx.delete(url, headers=headers)
+
+            if response.status_code == 200:
+                print(f'Букинг с ID {booking_id} успешно удален.')
+                self.bookings = [
+                    b for b in self.bookings if b['id'] != booking_id
+                ]
+                self.page.clean()
+                self.page.add(self.create_profile_page())
+                self.page.update()
+            else:
+                print(
+                    f'Ошибка при удалении букинга: '
+                    f'{response.status_code} - {response.text}'
+                )
+        except Exception as e:
+            print(f'Ошибка при запросе удаления букинга: {e}')
+
     def create_avatar_container(self):
-        avatar_url = self.page.client_storage.get('avatar_url') or None
+        avatar_url = self.avatar_url or None
 
         if avatar_url:
+            print('Аватар загружен из локальной памяти:', avatar_url)
             avatar_content = ft.Image(
                 src=avatar_url,
                 width=100,
@@ -290,6 +448,10 @@ class ProfilePage:
                 border_radius=ft.border_radius.all(50),
             )
         else:
+            print(
+                'Аватар не найден в локальной памяти. '
+                'Используем иконку по умолчанию.'
+            )
             avatar_content = ft.Icon(
                 ft.icons.PERSON,
                 size=100,
@@ -313,17 +475,100 @@ class ProfilePage:
 
     def on_picture_select(self, e: ft.FilePickerResultEvent):
         if e.files:
-            avatar_url = e.files[0].path
-            self.page.client_storage.set('avatar_url', avatar_url)
+            try:
+                with open(e.files[0].path, 'rb') as image_file:
+                    self.selected_image_bytes = image_file.read()
 
-            self.avatar_container.content = ft.Image(
-                src=avatar_url,
-                width=100,
-                height=100,
-                fit=ft.ImageFit.COVER,
-                border_radius=ft.border_radius.all(50),
+                avatar_url = e.files[0].path
+                self.avatar_container.content = ft.Image(
+                    src=avatar_url,
+                    width=100,
+                    height=100,
+                    fit=ft.ImageFit.COVER,
+                    border_radius=ft.border_radius.all(50),
+                )
+                self.page.update()
+
+                self.upload_avatar_to_server()
+
+            except Exception as ex:
+                print(f'Ошибка при открытии файла: {ex}')
+
+    def upload_avatar_to_server(self):
+        try:
+            user_id = self.page.client_storage.get('user_id')
+            access_token = self.page.client_storage.get('access_token')
+            if not user_id or not access_token:
+                print('User ID или access_token не найдены')
+                return
+
+            headers = {
+                'Authorization': f'Bearer {access_token}',
+                'Accept': 'application/json',
+            }
+
+            files = {
+                'image': ('avatar.png', io.BytesIO(self.selected_image_bytes))
+            }
+
+            new_values = {'username': self.username}
+
+            url = f"{self.api_url.rstrip('/')}/users/{user_id}"
+
+            response = httpx.patch(
+                url,
+                files=files,
+                data={'new_values': json.dumps(new_values)},
+                headers=headers,
             )
-            self.page.update()
+
+            if response.status_code == 200:
+                print('Аватар успешно загружен на сервер')
+                self.get_user_data()
+
+            else:
+                print(
+                    f'Ошибка загрузки аватара: '
+                    f'{response.status_code} - {response.text}'
+                )
+        except Exception as e:
+            print(f'Ошибка при загрузке аватара на сервер: {e}')
+
+    def get_user_data(self):
+        try:
+            access_token = self.page.client_storage.get('access_token')
+            headers = {
+                'Authorization': f'Bearer {access_token}',
+                'Accept': 'application/json',
+            }
+
+            url = f"{self.api_url.rstrip('/')}/users/me"
+
+            response = httpx.get(url, headers=headers)
+
+            if response.status_code == 200:
+                user_data = response.json()
+                avatar_url = user_data.get('image_link')
+                if avatar_url:
+                    print('Аватар загружен с сервера:', avatar_url)
+                    self.page.client_storage.set('avatar_url', avatar_url)
+                    self.avatar_url = avatar_url
+
+                    self.avatar_container.content = ft.Image(
+                        src=avatar_url,
+                        width=100,
+                        height=100,
+                        fit=ft.ImageFit.COVER,
+                        border_radius=ft.border_radius.all(50),
+                    )
+                    self.page.update()
+            else:
+                print(
+                    f'Ошибка при получении данных пользователя с сервера: '
+                    f'{response.status_code} - {response.text}'
+                )
+        except Exception as e:
+            print(f'Ошибка при запросе данных пользователя с сервера: {e}')
 
     def on_delete_avatar_click(self, e):
         self.page.client_storage.remove('avatar_url')
