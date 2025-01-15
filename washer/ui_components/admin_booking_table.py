eef07c1fdf7d4aac7aee3d78c6244c85b6f9d307
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2025-01-15 14:30:08 +0500

    add detailed information about bookings and improve UI in AdminBookingTable: expanded loading of booking data (username, car_name with licence_plate, phone number), improved display of the booking table with increased font and cell sizes, improved the visual component of the table with alternating colors and adding borders

--- washer/ui_components/admin_booking_table.py
+++ washer/ui_components/admin_booking_table.py
@@ -76,11 +76,28 @@ class AdminBookingTable:
                 self.bookings = bookings_data
 
                 for booking in self.bookings:
-                    if 'user_car_id' in booking:
-                        car_name = self.get_car_name(booking['user_car_id'])
-                        booking['car_name'] = (
-                            car_name if car_name != 'Неизвестно' else ''
+                    user_car = booking.get('user_car')
+                    if user_car:
+                        booking['car_name'] = user_car.get(
+                            'name', 'Неизвестно'
                         )
+                        booking['license_plate'] = user_car.get(
+                            'license_plate', '---'
+                        )
+                        user = user_car.get('user', {})
+                        booking['first_name'] = user.get(
+                            'first_name', 'Неизвестен'
+                        )
+                        booking['last_name'] = user.get('last_name', '')
+                        booking['phone_number'] = user.get(
+                            'phone_number', '---'
+                        )
+                    else:
+                        booking['car_name'] = 'Неизвестно'
+                        booking['license_plate'] = '---'
+                        booking['first_name'] = 'Неизвестен'
+                        booking['last_name'] = ''
+                        booking['phone_number'] = '---'
 
                 print(
                     f"Загружено букингов: {len(self.bookings)} "
@@ -193,7 +210,7 @@ class AdminBookingTable:
         if not self.schedule_data:
             print('Нет расписаний для отображения.')
             return ft.Text(
-                'Нет доступных расписаний.',
+                'Нет доступных расписований.',
                 size=18,
                 text_align=ft.TextAlign.CENTER,
             )
@@ -205,8 +222,8 @@ class AdminBookingTable:
             day_of_week = schedule.get('day_of_week')
             schedule_date = self.dates_storage.get(day_of_week)
             formatted_date = schedule_date.strftime('%d %B')
-            day_name = self.get_day_name(day_of_week)
-            day_with_date = f'{day_name} ({formatted_date})'
+            # Отображаем только дату без дня недели
+            day_with_date = f'{formatted_date}'
 
             if self.selected_date and schedule_date == self.selected_date:
                 selected_index = i
@@ -223,7 +240,11 @@ class AdminBookingTable:
                 else ft.Container()
             )
 
-            tabs.append(ft.Tab(text=day_name, content=tab_content))
+            tabs.append(
+                ft.Tab(
+                    text=self.get_day_name(day_of_week), content=tab_content
+                )
+            )
 
         if not tabs:
             print('Расписания есть, но вкладки не создались.')
@@ -274,23 +295,30 @@ class AdminBookingTable:
     def create_booking_table(self, day_with_date, schedule, timeslots):
         rows = []
 
+        black_border_bottom = ft.border.Border(
+            bottom=ft.border.BorderSide(width=1, color=ft.colors.BLACK),
+        )
+
         header = ft.Container(
             content=ft.Text(
                 day_with_date,
-                size=24,
-                weight='bold',
+                size=28,
+                weight=ft.FontWeight.BOLD,
                 text_align=ft.TextAlign.CENTER,
             ),
             alignment=ft.alignment.center,
             padding=ft.padding.only(bottom=10),
-            height=50,
+            height=60,
         )
         rows.append(header)
 
         box_names_row = [
             ft.Container(
                 content=ft.Text(
-                    '', weight='bold', text_align=ft.TextAlign.CENTER, size=14
+                    '',  # Пустой заголовок
+                    weight=ft.FontWeight.BOLD,
+                    text_align=ft.TextAlign.CENTER,
+                    size=16,
                 ),
                 width=100,
                 height=40,
@@ -303,9 +331,9 @@ class AdminBookingTable:
                 ft.Container(
                     content=ft.Text(
                         box['name'],
-                        weight='bold',
+                        weight=ft.FontWeight.BOLD,
                         text_align=ft.TextAlign.CENTER,
-                        size=14,
+                        size=16,
                     ),
                     expand=True,
                     height=40,
@@ -324,25 +352,62 @@ class AdminBookingTable:
         )
 
         skip_slots = {}
+
+        box_booking_counts = {box['id']: 0 for box in self.boxes_list}
+
+        def handle_booking_click(e, booking):
+            self.open_booking_details_dialog(booking)
+
         for time in timeslots:
-            row_controls = [ft.Text(time, weight='bold', width=100, height=40)]
+            row_controls = [
+                ft.Text(
+                    time,
+                    weight=ft.FontWeight.BOLD,
+                    width=100,
+                    height=40,
+                    size=16,
+                )
+            ]
             current_date = self.dates_storage[schedule['day_of_week']]
 
             for box in self.boxes_list:
-                if skip_slots.get(box['id'], 0) > 0:
-                    skip_slots[box['id']] -= 1
+                box_id = box['id']
+
+                if box_id in skip_slots:
+                    booking, occupied_color = skip_slots[box_id]
+
                     row_controls.append(
                         ft.Container(
-                            content=ft.Text(
-                                '', size=12, text_align=ft.TextAlign.CENTER
+                            content=ft.Column(
+                                [
+                                    ft.Text(
+                                        f"₸{booking.get('total_price', '0')}",
+                                        size=14,
+                                        text_align=ft.TextAlign.CENTER,
+                                    ),
+                                    ft.Text(
+                                        booking.get('phone_number', '---'),
+                                        size=14,
+                                        text_align=ft.TextAlign.CENTER,
+                                    ),
+                                ],
+                                alignment=ft.MainAxisAlignment.CENTER,
+                                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                                spacing=2,
                             ),
-                            bgcolor=ft.colors.TRANSPARENT,
+                            bgcolor=occupied_color,
                             padding=ft.padding.all(5),
                             alignment=ft.alignment.center,
                             expand=True,
-                            height=40,
+                            height=80,
+                            border=black_border_bottom,
+                            on_click=lambda e, b=booking: handle_booking_click(
+                                e, b
+                            ),
                         )
                     )
+
+                    del skip_slots[box_id]
                     continue
 
                 current_time_slot = f'{current_date}T{time}:00'
@@ -351,7 +416,7 @@ class AdminBookingTable:
                     (
                         b
                         for b in self.bookings
-                        if b['box_id'] == box['id']
+                        if b['box_id'] == box_id
                         and b['start_datetime']
                         <= current_time_slot
                         < b['end_datetime']
@@ -360,8 +425,58 @@ class AdminBookingTable:
                 )
 
                 if booking:
-                    color = ft.colors.GREY_500
-                    text = 'Занято'
+                    booking_count = box_booking_counts[box_id]
+                    occupied_color = (
+                        ft.colors.GREY_500
+                        if booking_count % 2 == 0
+                        else ft.colors.GREY_400
+                    )
+
+                    user_full_name = (
+                        f"{booking.get('first_name', '')} "
+                        f"{booking.get('last_name', '')}"
+                    ).strip()
+                    car_info = f"{booking.get('car_name', '')}"
+                    license_plate = f"({booking.get('license_plate', '---')})"
+
+                    row_controls.append(
+                        ft.Container(
+                            content=ft.Column(
+                                [
+                                    ft.Text(
+                                        user_full_name,
+                                        size=14,
+                                        weight=ft.FontWeight.BOLD,
+                                        text_align=ft.TextAlign.CENTER,
+                                        overflow=ft.TextOverflow.ELLIPSIS,
+                                    ),
+                                    ft.Text(
+                                        car_info,
+                                        size=14,
+                                        text_align=ft.TextAlign.CENTER,
+                                        overflow=ft.TextOverflow.ELLIPSIS,
+                                    ),
+                                    ft.Text(
+                                        license_plate,
+                                        size=14,
+                                        text_align=ft.TextAlign.CENTER,
+                                        overflow=ft.TextOverflow.ELLIPSIS,
+                                    ),
+                                ],
+                                alignment=ft.MainAxisAlignment.CENTER,
+                                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                                spacing=2,
+                            ),
+                            bgcolor=occupied_color,
+                            padding=ft.padding.all(5),
+                            alignment=ft.alignment.center,
+                            expand=True,
+                            height=80,
+                            on_click=lambda e, b=booking: handle_booking_click(
+                                e, b
+                            ),
+                        )
+                    )
 
                     start_time = datetime.datetime.fromisoformat(
                         booking['start_datetime']
@@ -369,10 +484,6 @@ class AdminBookingTable:
                     end_time = datetime.datetime.fromisoformat(
                         booking['end_datetime']
                     ).time()
-                    # в новом используется более удобный и "автоматический"
-                    # метод datetime.datetime.fromisoformat, который позволяет
-                    # обойтись без явного указания формата, если строка
-                    # соответствует стандартному ISO-формату даты и времени.
                     duration_slots = int(
                         (
                             datetime.datetime.combine(
@@ -384,29 +495,15 @@ class AdminBookingTable:
                         ).seconds
                         / 3600
                     )
-                    skip_slots[box['id']] = duration_slots - 1
+                    if duration_slots > 1:
+                        skip_slots[box_id] = (booking, occupied_color)
 
-                    row_controls.append(
-                        ft.Container(
-                            content=ft.Text(
-                                text, size=12, text_align=ft.TextAlign.CENTER
-                            ),
-                            bgcolor=color,
-                            padding=ft.padding.all(5),
-                            alignment=ft.alignment.center,
-                            expand=True,
-                            height=40,
-                            border_radius=ft.border_radius.all(15),
-                            on_click=lambda e,
-                            booking=booking: self.open_booking_details_dialog(
-                                booking
-                            ),
-                        )
-                    )
+                    box_booking_counts[box_id] += 1
+
                 else:
                     box_times = self.available_times.get(
                         str(current_date), {}
-                    ).get(str(box['id']), [])
+                    ).get(str(box_id), [])
                     is_available = any(
                         start_time <= current_time_slot < end_time
                         for start_time, end_time in box_times
@@ -414,48 +511,50 @@ class AdminBookingTable:
 
                     if is_available:
                         color = ft.colors.BLUE
-                        text = 'Свободно'
                         row_controls.append(
                             ft.Container(
                                 content=ft.Text(
-                                    text,
-                                    size=12,
+                                    'Свободно',
+                                    size=14,
                                     text_align=ft.TextAlign.CENTER,
                                 ),
                                 bgcolor=color,
                                 padding=ft.padding.all(5),
                                 alignment=ft.alignment.center,
                                 expand=True,
-                                height=40,
-                                border_radius=ft.border_radius.all(15),
+                                height=80,
+                                border=black_border_bottom,
                                 on_click=lambda e,
-                                box_id=box['id'],
-                                time_slot=time,
-                                date=current_date: self.open_booking_page(
-                                    box_id, date, time_slot
+                                b_id=box_id,
+                                t_slot=time,
+                                d=current_date: self.open_booking_page(
+                                    b_id, d, t_slot
                                 ),
                             )
                         )
                     else:
-                        color = ft.colors.GREY_600
-                        text = 'Не актуально'
+                        color = ft.colors.TRANSPARENT
                         row_controls.append(
                             ft.Container(
                                 content=ft.Text(
-                                    text,
-                                    size=12,
+                                    'Не актуально',
+                                    size=14,
                                     text_align=ft.TextAlign.CENTER,
                                 ),
                                 bgcolor=color,
                                 padding=ft.padding.all(5),
                                 alignment=ft.alignment.center,
                                 expand=True,
-                                height=40,
-                                border_radius=ft.border_radius.all(15),
+                                height=80,
+                                border=black_border_bottom,
+                                on_click=lambda e,
+                                b=booking: handle_booking_click(e, b)
+                                if b
+                                else None,
                             )
                         )
 
-            row = ft.Row(controls=row_controls, spacing=5, height=50)
+            row = ft.Row(controls=row_controls, spacing=5, height=80)
             rows.append(ft.Container(content=row))
 
         return ft.ListView(
@@ -474,18 +573,15 @@ class AdminBookingTable:
         return ''
 
     def open_booking_details_dialog(self, booking):
-        user_id = booking.get('user_id')
-        car_name = booking.get('car_name', '')
-        price = booking.get('price', 'Не указана')
+        first_name = booking.get('first_name', 'Неизвестен')
+        last_name = booking.get('last_name', '')
+        phone_number = booking.get('phone_number', '---')
+        car_name = booking.get('car_name', 'Неизвестно')
+        price = booking.get('total_price', 'Не указана')
 
-        user_response = self.api.get_user_by_id(user_id)
-        if user_response and user_response.status_code == 200:
-            user_data = user_response.json()
-            first_name = user_data.get('first_name', '')
-            last_name = user_data.get('last_name', '')
-            full_name = f'{first_name} {last_name}'.strip()
-        else:
-            full_name = 'Неизвестен'
+        full_name = (
+            f'{first_name} {last_name}'.strip() if last_name else first_name
+        )
 
         def confirm_delete(e):
             self.page.close(confirm_dialog)
@@ -517,7 +613,7 @@ class AdminBookingTable:
                     self.create_detail_row(ft.icons.PERSON, full_name),
                     self.create_detail_row(ft.icons.DIRECTIONS_CAR, car_name),
                     self.create_detail_row(ft.icons.MONEY, f'₸{price}'),
-                    self.create_detail_row(ft.icons.PHONE, ''),
+                    self.create_detail_row(ft.icons.PHONE, phone_number),
                 ]
             ),
             actions=[
