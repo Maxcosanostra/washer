46bc26e19c13dfe8ce0a5c6056fc3296310de409
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2025-01-30 23:15:33 +0500

    change font src

--- washer/main.py
+++ washer/main.py
@@ -6,7 +6,8 @@ from washer.ui_components.sign_up_page import SignUpPage
 
 def main(page: ft.Page):
     page.fonts = {
-        'LavishlyYours': 'https://drive.google.com/uc?export=view&id=17uMDY7jYszJZS3td3pLzhnPbeKIXnZTD'
+        'LavishlyYours': 'http://77.73.66.191:9001/api/v1/buckets/general-bucket/objects/download?preview=true&prefix=LavishlyYours-Regular.ttf&version_id=null'
+        # 'LavishlyYours': 'https://drive.google.com/uc?export=view&id=17uMDY7jYszJZS3td3pLzhnPbeKIXnZTD'
     }
 
     page.api = BackendApi()
