d23f9b0552c45170e3e7e5b33547ad566d7f562e
Maxcosanostra <155756786+Maxcosanostra@users.noreply.github.com>
2025-02-01 03:10:34 +0500

    change 'back' text to TextButton/ add self.page.floating_action_button = None to __init__

--- washer/ui_components/carwash_edit_page.py
+++ washer/ui_components/carwash_edit_page.py
@@ -30,6 +30,8 @@ class CarWashEditPage:
         self.show_cancel_button = False
         self.page.adaptive = True
 
+        self.page.floating_action_button = None
+
         app_bar = ft.AppBar(
             leading=ft.Row(
                 controls=[
@@ -39,7 +41,14 @@ class CarWashEditPage:
                         icon_color='#ef7b00',
                         padding=ft.padding.only(left=10),
                     ),
-                    ft.Text('Назад', size=16, color='#ef7b00'),
+                    ft.TextButton(
+                        text='Назад',
+                        on_click=self.on_back_to_admin_page,
+                        style=ft.ButtonStyle(
+                            padding=0,
+                            color='#ef7b00',
+                        ),
+                    ),
                 ],
                 alignment=ft.MainAxisAlignment.START,
             ),
